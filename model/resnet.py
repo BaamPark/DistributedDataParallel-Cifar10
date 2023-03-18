ff977b153dcfa7c4e9d7f8b12ae96ecf6bc15510
BaamPark <baam.park.95@gmail.com>
2023-03-18 16:27:54 -0400

    code works

--- model/resnet.py
+++ model/resnet.py
@@ -1,6 +1,25 @@
+import torch.nn as nn
 import torch
 import torch.nn.functional as F
-import torch.nn as nn
+
+class NetResDeep(nn.Module):
+    def __init__(self, n_chans1=32, n_blocks=10):
+        super().__init__()
+        self.n_chans1 = n_chans1
+        self.conv1 = nn.Conv2d(3, n_chans1, kernel_size=3, padding=1)
+        self.resblocks = nn.Sequential(
+            *(n_blocks * [ResBlock(n_chans=n_chans1)]))
+        self.fc1 = nn.Linear(8 * 8 * n_chans1, 32)
+        self.fc2 = nn.Linear(32, 10)
+        
+    def forward(self, x):
+        out = F.max_pool2d(torch.relu(self.conv1(x)), 2)
+        out = self.resblocks(out)
+        out = F.max_pool2d(out, 2)
+        out = out.view(-1, 8 * 8 * self.n_chans1)
+        out = torch.relu(self.fc1(out))
+        out = self.fc2(out)
+        return out
 
 class ResBlock(nn.Module):
     def __init__(self, n_chans):
@@ -16,23 +35,3 @@ class ResBlock(nn.Module):
         out = self.batch_norm(out)
         out = torch.relu(out)
         return out + x
-
-class NetResDeep(nn.Module):
-    def __init__(self, n_chans1=32, n_blocks=10):
-        super().__init__()
-        self.n_chans1 = n_chans1
-        self.conv1 = nn.Conv2d(3, n_chans1, kernel_size=3, padding=1)
-        self.resblocks = nn.Sequential(
-            *(n_blocks * [ResBlock(n_chans=n_chans1)]))
-        self.fc1 = nn.Linear(8 * 8 * n_chans1, 32)
-        self.fc2 = nn.Linear(32, 2)
-        
-    def forward(self, x):
-        out = F.max_pool2d(torch.relu(self.conv1(x)), 2)
-        out = self.resblocks(out)
-        out = F.max_pool2d(out, 2)
-        out = out.view(-1, 8 * 8 * self.n_chans1)
-        out = torch.relu(self.fc1(out))
-        out = self.fc2(out)
-        return out
-    
